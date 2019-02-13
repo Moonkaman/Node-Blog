4f3c6b7fadd591b6226ab1a442b25e7d8005e379
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-13 13:38:39 -0700

    Setup express server and some middleware

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code away!
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+
+const server = express();
+
+server.use(morgan('dev'));
+server.use(helmet());
+
+server.listen(8000, _ => console.log('\n***Server is running on port 8000***\n'));
