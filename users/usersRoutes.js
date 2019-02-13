c907949b261c94e9a70c6dab4f24fe1b304b0293
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-13 14:00:09 -0700

    Create a post endpoint to /users

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -11,6 +11,16 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({errorMessage: 'Could not retrieve users at this time.', error: err}));
 });
 
+router.post('/', (req, res) => {
+  if(!req.body.name) {
+    res.status(400).json({errorMessage: 'Please provide a name'});
+  } else {
+  userDb.insert(req.body)
+    .then(newUser => res.status(201).json(newUser))
+    .catch(err => res.status(500).json({errorMessage: 'Could not create a user at this time.', error: err}));
+  }
+})
+
 router.get('/:id', (req, res) => {
   userDb.getById(req.params.id)
     .then(user => user ? res.status(200).json(user) : res.status(404).json({errorMessage: `User id:${req.params.id} not found`}))
