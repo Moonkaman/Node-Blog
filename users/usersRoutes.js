a8e3124d13ae4435a1e2a8cf0f26efe7b6f1387d
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-13 14:11:12 -0700

    Created put endpoint for /users/:id

--- users/usersRoutes.js
+++ users/usersRoutes.js
@@ -21,16 +21,26 @@ router.post('/', (req, res) => {
   }
 })
 
+router.get('/:id', (req, res) => {
+  userDb.getById(req.params.id)
+    .then(user => user ? res.status(200).json(user) : res.status(404).json({errorMessage: `User id:${req.params.id} not found`}))
+    .catch(err => res.status(500).json({errorMessage: `Could not retrieve user id:${req.params.id} at this time.`, error: err}));
+});
+
 router.delete('/:id', (req, res) => {
   userDb.remove(req.params.id)
     .then(count => count < 1 ? res.status(404).json({errorMessage: 'The user you tried to delete could not be found'}) : res.status(200).json({message: `You successfully deleted ${count} user`}))
     .catch(err => res.status(500).json({errorMessage: `Could not delete user id:${req.params.id} at this time.`, error: err}));
 })
 
-router.get('/:id', (req, res) => {
-  userDb.getById(req.params.id)
-    .then(user => user ? res.status(200).json(user) : res.status(404).json({errorMessage: `User id:${req.params.id} not found`}))
-    .catch(err => res.status(500).json({errorMessage: `Could not retrieve user id:${req.params.id} at this time.`, error: err}));
+router.put('/:id', (req, res) => {
+  if(!req.body.name) {
+    res.status(400).json({errorMessage: 'Please provide a name'});
+  } else {
+  userDb.update(req.params.id, req.body)
+    .then(count => count > 0 ? userDb.getById(req.params.id).then(user => res.status(200).json(user)).catch(err => res.status(500).json({errorMessage: 'Could not retrieve user at this time.', error: err})) : res.status(404).json({errorMessage: `User id:${req.params.id} not found`}))
+    .catch(err => res.status(500).json({errorMessage: `Could not update user id:${req.params.id} at this time.`, error: err}));
+  }
 });
 
 module.exports = router;
